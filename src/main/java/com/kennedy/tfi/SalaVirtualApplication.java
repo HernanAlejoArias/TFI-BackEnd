f5e92728bc1f67dabdd43d5fd63a36531b52d028
HernanAlejoArias <harias1982@gmail.com>
2021-05-21 15:15:01 -0300

    Test Case 1 complet
    All the data for the test case 1 is uploaded on the CommandLineRunner

--- src/main/java/com/kennedy/tfi/SalaVirtualApplication.java
+++ src/main/java/com/kennedy/tfi/SalaVirtualApplication.java
@@ -63,7 +63,7 @@ public class SalaVirtualApplication {
 			patientRepository.save(tempPatient);
 
 			// Appointment
-			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(9, 0), false, LocalTime.of(9, 20),
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(9, 0), false, LocalTime.of(9, 15),
 					tempClinica, tempPatient, true, false);
 			appointmentRepository.save(tempAppointment);
 
@@ -71,7 +71,7 @@ public class SalaVirtualApplication {
 					false, false, false);
 			patientRepository.save(tempPatient);
 
-			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(9, 20), false, LocalTime.of(9, 40),
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(9, 15), false, LocalTime.of(9, 30),
 					tempClinica, tempPatient, true, false);
 			appointmentRepository.save(tempAppointment);
 
@@ -79,7 +79,7 @@ public class SalaVirtualApplication {
 					false, false, false, false);
 			patientRepository.save(tempPatient);
 
-			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(9, 41), false, LocalTime.of(9, 55),
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(9, 30), false, LocalTime.of(9, 45),
 					tempClinica, tempPatient, true, false);
 			appointmentRepository.save(tempAppointment);
 
@@ -87,128 +87,275 @@ public class SalaVirtualApplication {
 					false, false, false, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(9, 45), false, LocalTime.of(10, 0),
+					tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Victoria", "Arias", LocalDate.of(1980, 1, 25), 26742442, 2, "Lomas", true, false,
 					false, false, false);
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(10, 0), false,
+					LocalTime.of(10, 15), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 
 			tempPatient = new Patient("Camila", "Garcia", LocalDate.of(1982, 7, 9), 30333982, 2, "Lomas", false, false,
 					true, false, true);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(10, 15), false,
+					LocalTime.of(10, 30), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Gabriela", "Suarez", LocalDate.of(1981, 11, 11), 28447445, 2, "CABA", true,
 					false, false, false, false);
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(10, 30), false,
+					LocalTime.of(10, 45), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 
 			tempPatient = new Patient("Francisco", "De Marco", LocalDate.of(1978, 9, 10), 25725839, 1, "CABA", true,
 					true, false, false, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(10, 45), false,
+					LocalTime.of(11, 0), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Emilio", "Dissi", LocalDate.of(1970, 3, 3), 20876091, 1, "Lomas", true, true,
 					true, true, false);
 			patientRepository.save(tempPatient);
-			// 10
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(11, 0), false,
+					LocalTime.of(11, 15), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+			// 10
 			tempPatient = new Patient("Iris", "De Ojo", LocalDate.of(1985, 9, 1), 33445775, 2, "Lanus", true, false,
 					false, false, false);
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(11, 0), false,
+					LocalTime.of(11, 15), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 
 			tempPatient = new Patient("Luciano", "Lunga", LocalDate.of(1981, 11, 11), 22987456, 1, "Lomas", true, false,
 					false, false, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(11, 15), false,
+					LocalTime.of(11, 30), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Alejo", "Perez", LocalDate.of(1982, 2, 15), 29851753, 1, "Temperley", false,
 					false, false, true, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(11, 30), false,
+					LocalTime.of(11, 45), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Paula", "Perez", LocalDate.of(1980, 2, 25), 29356543, 2, "Lanus", false, true,
 					false, false, false);
+
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(11, 45), false,
+					LocalTime.of(12, 0), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 
 			tempPatient = new Patient("Julio", "Sanchez", LocalDate.of(1955, 5, 15), 12615229, 1, "Lanus", false, false,
 					false, false, false);
 			patientRepository.save(tempPatient);
 
+			appointmentRepository.save(tempAppointment);
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(12, 0), false,
+					LocalTime.of(12, 15), tempClinica, tempPatient, true, false);
+
 			tempPatient = new Patient("Laura", "Bonilla", LocalDate.of(1984, 5, 15), 33153323, 2, "Lanus", false, false,
 					false, false, false);
 			patientRepository.save(tempPatient);
+
+			appointmentRepository.save(tempAppointment);
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(12, 15), false,
+					LocalTime.of(12, 30), tempClinica, tempPatient, true, false);
 
 			tempPatient = new Patient("Andrea", "Gonzales", LocalDate.of(1982, 2, 27), 29445256, 2, "Temperlay", true,
 					true, false, false, false);
 			patientRepository.save(tempPatient);
 
+			appointmentRepository.save(tempAppointment);
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(12, 30), false,
+					LocalTime.of(12, 45), tempClinica, tempPatient, true, false);
+
 			tempPatient = new Patient("Olivia", "Perez", LocalDate.of(1952, 7, 7), 9234568, 2, "Lanus", true, false,
 					false, false, false);
 			patientRepository.save(tempPatient);
+
+			appointmentRepository.save(tempAppointment);
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(12, 45), false,
+					LocalTime.of(13, 0), tempClinica, tempPatient, true, false);
 
 			tempPatient = new Patient("Andres", "Maldini", LocalDate.of(1981, 1, 19), 28567532, 1, "Banfield", true,
 					false, false, false, false);
 			patientRepository.save(tempPatient);
 
+			appointmentRepository.save(tempAppointment);
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(13, 0), false,
+					LocalTime.of(13, 15), tempClinica, tempPatient, true, false);
+
 			tempPatient = new Patient("Sebastian", "Titolo", LocalDate.of(1980, 7, 3), 26123982, 1, "Lanus", true,
 					false, false, false, false);
 			patientRepository.save(tempPatient);
-			// 20
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(13, 15), false,
+					LocalTime.of(13, 30), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+			// 20
 			tempPatient = new Patient("Camila", "Ensilla", LocalDate.of(1990, 10, 2), 37091840, 2, "Lomas", false,
 					false, true, false, true);
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(13, 30), false,
+					LocalTime.of(13, 45), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 
 			tempPatient = new Patient("Roberto", "Burgos", LocalDate.of(1982, 7, 7), 290911112, 1, "CABA", true, false,
 					false, false, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(13, 45), false,
+					LocalTime.of(14, 0), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Hugo", "Fernandez", LocalDate.of(1977, 9, 10), 25763201, 1, "CABA", true, true,
 					false, false, false);
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(14, 0), false,
+					LocalTime.of(14, 15), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 
 			tempPatient = new Patient("German", "Castro", LocalDate.of(1967, 1, 28), 18888881, 1, "Lomas", true, true,
 					true, true, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(14, 15), false,
+					LocalTime.of(14, 30), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Sebastian", "Palmiero", LocalDate.of(1979, 9, 10), 261117625, 1, "CABA", true,
 					false, false, false, false);
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(14, 30), false,
+					LocalTime.of(14, 45), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 
 			tempPatient = new Patient("Gustavo", "Bermudez", LocalDate.of(1986, 6, 28), 33554109, 1, "Lomas", true,
 					false, false, false, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(14, 45), false,
+					LocalTime.of(15, 0), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Anabela", "Lucia", LocalDate.of(1983, 8, 12), 31311256, 1, "Temperley", false,
 					false, false, true, false);
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(15, 0), false,
+					LocalTime.of(15, 15), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 
 			tempPatient = new Patient("Sebastian", "Saccani", LocalDate.of(1985, 12, 12), 30137911, 1, "CABA", true,
 					false, false, false, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(15, 15), false,
+					LocalTime.of(15, 30), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Natalia", "Saccani", LocalDate.of(1989, 6, 13), 36009272, 2, "Lanus", true,
 					false, false, false, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(15, 30), false,
+					LocalTime.of(15, 45), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Natalia", "Damele", LocalDate.of(1990, 1, 13), 40192827, 1, "Lanus", false,
 					false, false, true, false);
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(15, 45), false,
+					LocalTime.of(16, 0), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 			// 30
 
 			tempPatient = new Patient("Eliana", "Matera", LocalDate.of(1994, 11, 19), 45029126, 1, "CABA", true, false,
 					false, false, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(16, 0), false,
+					LocalTime.of(16, 15), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Florencia", "Persiani", LocalDate.of(1986, 9, 20), 33245927, 2, "Lomas", true,
 					false, false, false, false);
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(16, 15), false,
+					LocalTime.of(16, 30), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 
 			tempPatient = new Patient("Florencia", "De Lucca", LocalDate.of(1991, 5, 12), 37726189, 2, "Lanus", false,
 					false, false, true, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(16, 30), false,
+					LocalTime.of(16, 45), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Norma", "Ponce", LocalDate.of(1968, 5, 4), 13916645, 1, "CABA", true, false,
 					false, false, false);
 			patientRepository.save(tempPatient);
 
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(16, 45), false,
+					LocalTime.of(17, 0), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
 			tempPatient = new Patient("Florencia", "Salamanca", LocalDate.of(1995, 6, 13), 46782722, 2, "Lanus", true,
 					false, false, false, false);
 			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(17, 0), false,
+					LocalTime.of(17, 15), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
+			tempPatient = new Patient("Vanesa", "Apodaca", LocalDate.of(1982, 5, 14), 29786687, 2, "Lanus", true, false,
+					false, false, false);
+			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(17, 15), false,
+					LocalTime.of(17, 30), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
+			tempPatient = new Patient("Gabriel", "Perez", LocalDate.of(1988, 3, 27), 35781115, 1, "Lanus", true, false,
+					false, false, false);
+			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(17, 30), false,
+					LocalTime.of(17, 45), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
+
+			tempPatient = new Patient("Gonzalo", "Marcec", LocalDate.of(1984, 7, 10), 31766985, 1, "Lanus", true, false,
+					false, false, false);
+			patientRepository.save(tempPatient);
+
+			tempAppointment = new Appointment(LocalDate.of(2021, 6, 1), LocalTime.of(17, 45), false,
+					LocalTime.of(18, 0), tempClinica, tempPatient, true, false);
+			appointmentRepository.save(tempAppointment);
 			// 35
 
 			// Free Appointments
